6c90d8fd9a822b386fd6d84dbb4b94312e501fb8
Gene De Lisa <gene@rockhoppertech.com>
2015-11-22 07:43:45 -0500

    Initial

--- LimtedTableExample/ViewController.swift
+++ LimtedTableExample/ViewController.swift
@@ -9,6 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let intervalCellIdentifier = "intervalCellIdentifier"
+    
+    let intervalNames = [
+        "Unison",
+        "Minor Second",
+        "Major Second",
+        "Minor Third",
+        "Major Third",
+        "Perfect Fourth",
+        "Tritone",
+        "Perfect Fifth",
+        "Minor Sixth",
+        "Major Sixth",
+        "Minor seventh",
+        "Major Seventh",
+        "Octave",
+        "Minor Ninth",
+        "Major Ninth",
+        "Minor Tenth",
+        "Major Tenth",
+        "Eleventh",
+        "Augmented Eleventh",
+        "Minor Thirteenth",
+        "Major Thirteenth",
+        "Augmented Thirteenth",
+    ]
+    
+    let limit = 3
+    
+    var numSelectedIntervals = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,3 +55,71 @@ class ViewController: UIViewController {
 
 }
 
+//MARK: - UITableViewDataSource
+
+extension ViewController : UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return intervalNames.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(intervalCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        cell.accessoryType = .None
+        cell.textLabel?.text = intervalNames[indexPath.row]
+        
+        return cell
+    }
+    
+}
+
+//MARK: - UITableViewDelegate
+
+extension ViewController : UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        
+        if numSelectedIntervals == limit {
+            let alertController = UIAlertController(title: "Oops", message:
+                "You are limited to \(limit) selections", preferredStyle: .Alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: {action in
+            }))
+            self.presentViewController(alertController, animated: true, completion: nil)
+
+            return nil
+        }
+        return indexPath
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        print("selected  \(intervalNames[indexPath.row])")
+
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            if cell.selected {
+                cell.accessoryType = .Checkmark
+                numSelectedIntervals++
+            }
+        }
+        
+        if let sr = tableView.indexPathsForSelectedRows {
+            print("didDeselectRowAtIndexPath selected rows:\(sr)")
+        }
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        print("deselected  \(intervalNames[indexPath.row])")
+        
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            cell.accessoryType = .None
+            numSelectedIntervals--
+        }
+        
+        if let sr = tableView.indexPathsForSelectedRows {
+            print("didDeselectRowAtIndexPath selected rows:\(sr)")
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,45 @@
+# Limited Table Example
+
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://en.wikipedia.org/wiki/MIT_License)
+[![Build Status](https://travis-ci.org/genedelisa/LimitedTableExample.svg)](https://travis-ci.org/genedelisa/LimitedTableExample)
+[![GitHub tag](https://img.shields.io/github/tag/genedelisa/LimitedTableExample.svg)](https://github.com/genedelisa/LimitedTableExample/)
+[![GitHub release](https://img.shields.io/github/release/genedelisa/LimitedTableExample.svg)](https://github.com/genedelisa/LimitedTableExample/)
+[![GitHub commits](https://img.shields.io/github/commits-since/genedelisa/LimitedTableExample/2.0.0.svg)
+
+
+A simple of a multiselect UITableView with a limited number of selections.
+
+
+## Blog post for this example.
+
+[Blog post](http://www.rockhoppertech.com/blog/)
+
+
+
+## Bugs
+
+
+[![GitHub issues](https://img.shields.io/github/issues/genedelisa/LimitedTableExample.svg)](https://github.com/genedelisa/LimitedTableExample/issues)
+
+If you find one, [please add it to issues](https://github.com/genedelisa/LimitedTableExample/issues)
+
+### Buy my kitty Giacomo some cat food
+
+If this project is of any use to you, my ravenous Maine Coon kitty would like a snack.
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=F5KE9Z29MH8YQ&bnP-DonationsBF:btn_donate_SM.gif:NonHosted)
+
+<img src="http://www.rockhoppertech.com/blog/wp-content/uploads/2015/05/IMG_0657.png" alt="Giacomo Kitty" width="400" height="300">
+
+
+## Licensing
+
+[MIT](https://en.wikipedia.org/wiki/MIT_License)
+
+Please read the [LICENSE](LICENSE) for details.
+
+## Credits
+
+*	[Gene De Lisa's development blog](http://rockhoppertech.com/blog/)
+*	[Gene De Lisa's music blog](http://genedelisa.com/)
+*   Twitter: [@GeneDeLisaDev](http://twitter.com/genedelisadev)
